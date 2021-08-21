16888b22979780559c38b129389b8a778355b4a5
Christian Frommeyer <christian.frommeyer@web.de>
2021-08-21 15:11:48 +0200

    Initial demo application

---
+++ opentelemetry-refresh/src/main/java/com/example/demo/OpentelemetryRefreshApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpentelemetryRefreshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpentelemetryRefreshApplication.class, args);
+	}
+
+}
